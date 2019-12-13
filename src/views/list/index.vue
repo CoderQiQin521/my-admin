73a8aab534377b4584606e627d50c21880780708
CoderQiQin521 <coderqiqin@aliyun.com>
2019-12-13 18:04:28 +0800

    列表

---
+++ src/views/list/index.vue
@@ -0,0 +1,92 @@
+<template>
+  <div class="app-container">
+    <el-form :inline="true" :model="formInline" class="demo-form-inline">
+      <el-form-item label="时间">
+        <el-date-picker type="date" placeholder="选择日期" style="width: 100%;" />
+      </el-form-item>
+      <el-form-item label="审批人">
+        <el-input v-model="formInline.user" placeholder="审批人" />
+      </el-form-item>
+      <el-form-item label="活动区域">
+        <el-select v-model="formInline.region" placeholder="活动区域">
+          <el-option label="区域一" value="shanghai" />
+          <el-option label="区域二" value="beijing" />
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" @click="onSubmit">查询</el-button>
+        <el-button type="success" @click="onSubmit">新建</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-table :data="tableData" border fit style="width: 100%">
+      <el-table-column prop="name" label="序号" width="180" />
+      <el-table-column prop="apple" label="标题" />
+      <el-table-column prop="banana" label="浏览量" width="180" />
+      <el-table-column prop="orange" label="创建时间" width="180" />
+      <el-table-column prop="orange" label="修改时间" width="180" />
+      <el-table-column label="操作" fixed="right">
+        <template slot-scope="scope">
+          <el-button size="mini" @click="$router.push(`/ads/edit/${scope.row._id}`)">编辑</el-button>
+          <el-button size="mini" type="danger" @click="remove(scope.row)">删除</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <pagination
+      v-show="total>0"
+      :total="total"
+      :page.sync="listQuery.page"
+      :limit.sync="listQuery.limit"
+      @pagination="getList"
+    />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      formInline: {
+        user: '',
+        region: ''
+      },
+      tableData: [
+        {
+          name: 'fruit-1',
+          apple: 'apple-10',
+          banana: 'banana-10',
+          orange: 'orange-10'
+        },
+        {
+          name: 'fruit-1',
+          apple: 'apple-10',
+          banana: 'banana-10',
+          orange: 'orange-10'
+        },
+        {
+          name: 'fruit-1',
+          apple: 'apple-10',
+          banana: 'banana-10',
+          orange: 'orange-10'
+        },
+        {
+          name: 'fruit-1',
+          apple: 'apple-10',
+          banana: 'banana-10',
+          orange: 'orange-10'
+        },
+        {
+          name: 'fruit-2',
+          apple: 'apple-20',
+          banana: 'banana-20',
+          orange: 'orange-20'
+        }
+      ]
+    }
+  }
+}
+</script>
+
+<style>
+</style>
